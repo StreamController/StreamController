f932f3a5dcfe056fba399e69b73667fb773f8067
Core447 <jmulmann@protonmail.com>
2023-10-03 12:38:59 +0200

    Add loguru logging

--- main.py
+++ main.py
@@ -12,11 +12,30 @@ This programm comes with ABSOLUTELY NO WARRANTY!
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
+# Import Python modules
 import sys
+from loguru import logger as log
 
 # Import own modules
 from src.app import App
 
+def config_logger():
+    log.remove(0)
+    # Create log files
+    log.add("logs/logs.log", rotation="3 days", backtrace=True, diagnose=True, level="TRACE")
+    # Set min level to print
+    log.add(sys.stderr, level="INFO")
+
+class Main:
+    def __init__(self, application_id):
+        # Launch gtk application
+        app = App(application_id=application_id)
+        app.run(sys.argv)
+
+
 if __name__ == "__main__":
-    app = App(application_id="com.core447.StreamController")
-    app.run(sys.argv)
+    config_logger()
+    log.info("Loading app")
+    Main(application_id="com.core447.StreamController")
+
+log.trace("Reached end of main.py")
--- src/app.py
+++ src/app.py
@@ -12,10 +12,14 @@ This programm comes with ABSOLUTELY NO WARRANTY!
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
+# Import gtk modules
 import gi
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw
+
+# Import Python modules
+from loguru import logger as log
 
 # Import own modules
 from src.windows.mainWindow import MainWindow
@@ -26,5 +30,8 @@ class App(Adw.Application):
         self.connect("activate", self.on_activate)
 
     def on_activate(self, app):
+        log.trace("running: on_activate")
         self.main_win = MainWindow(application=app)
         self.main_win.present()
+
+        log.success("Finished loading app")

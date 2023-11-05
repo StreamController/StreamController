852e0a1ada6dfe0246f32492e03ef103929b7bb5
Core447 <jmulmann@protonmail.com>
2023-11-05 14:41:48 +0100

    Add click hint to icon selector

--- src/windows/mainWindow/elements/RightArea/elements/IconSelector.py
+++ src/windows/mainWindow/elements/RightArea/elements/IconSelector.py
@@ -33,11 +33,38 @@ class IconSelector(Gtk.Box):
 
     def build(self):
         self.button = Gtk.Button(label="Select", css_classes=["icon-selector", "key-image", "no-padding"])
-        self.image = Gtk.Image(overflow=Gtk.Overflow.HIDDEN, css_classes=["key-image"])
-        self.button.set_child(self.image)
+
+        self.button_fixed = Gtk.Fixed()
+        self.button.set_child(self.button_fixed)
+
+        self.image = Gtk.Image(overflow=Gtk.Overflow.HIDDEN, css_classes=["key-image", "icon-selector-image-normal"])
+        # self.button.set_child(self.image)
+        self.button_fixed.put(self.image, 0, 0)
+
+        self.label = Gtk.Label(label="Click to change", css_classes=["icon-selector-hint-label-hidden"])
+        label_size = self.label.get_preferred_size()[1] # 1 for natural size
+        label_width, label_height = label_size.width, label_size.height
+        self.button_fixed.put(self.label, (175-label_width)/2, (175-label_height)/2) # 175 for the size of the image
+
+        # Hover controller - css doesn't work because a :hover on the image would leave if focus switches to label
+        motion_controller = Gtk.EventControllerMotion()
+        motion_controller.connect("enter", self.on_hover_enter)
+        motion_controller.connect("leave", self.on_hover_leave)
+
+        self.button.add_controller(motion_controller)
 
         self.append(self.button)
 
     def set_image(self, image):
         pixbuf = image2pixbuf(image.convert("RGBA"), force_transparency=True)
         GLib.idle_add(self.image.new_from_pixbuf, pixbuf)
+
+    def on_hover_enter(self, *args):
+        self.label.set_css_classes(["icon-selector-hint-label-visible"])
+        self.image.remove_css_class("icon-selector-image-normal")
+        self.image.add_css_class("icon-selector-image-hover")
+
+    def on_hover_leave(self, *args):
+        self.label.set_css_classes(["icon-selector-hint-label-hidden"])
+        self.image.remove_css_class("icon-selector-image-hover")
+        self.image.add_css_class("icon-selector-image-normal")

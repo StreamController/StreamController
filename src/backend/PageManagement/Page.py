3d5cf6ebdb993694a4b69302fd1dff02c25b12df
Core447 <jmulmann@protonmail.com>
2023-11-11 17:15:56 +0100

    Fix: Bug in Page.has_key_an_image_controlling_action

--- src/backend/PageManagement/Page.py
+++ src/backend/PageManagement/Page.py
@@ -136,11 +136,12 @@ class Page(dict):
                 if self.action_objects[coords][i] == action_object:
                     self["keys"][coords]["actions"][i]["settings"] = settings
 
-    def has_key_a_image_controlling_action(self, page_coords: str):
-        for key in self.action_objects:
-            for action in self.action_objects[key].values():
-                if action.CONTROLS_KEY_IMAGE:
-                    return True
+        if page_coords not in self.action_objects:
+            return False
+        for action in self.action_objects[page_coords].values():
+            if action.CONTROLS_KEY_IMAGE:
+                print(action)
+                return True
         return False
 
     def call_actions_ready(self):

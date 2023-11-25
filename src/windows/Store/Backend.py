1dee23b297b307e5f1d4ec425ad1b2e9ae70449a
Core447 <jmulmann@protonmail.com>
2023-11-25 18:32:09 +0100

    Remove absolute paths

--- src/windows/Store/Backend.py
+++ src/windows/Store/Backend.py
@@ -150,7 +150,7 @@ class StoreBackend:
         
         ## Save to cache
         image_uuid = str(uuid.uuid4())
-        save_path = f"/mnt/P3/Development/Programmieren/Python/StreamController/src/windows/Store/cache/images/{self.get_repo_name(url)}::{image_uuid}.png"
+        save_path = f"src/windows/Store/cache/images/{self.get_repo_name(url)}::{image_uuid}.png"
         img.save(save_path)
         if url in self.image_cache:
             # Remove the old file
@@ -158,7 +158,7 @@ class StoreBackend:
                 os.remove(self.image_cache[url])
         self.image_cache[url] = save_path
         # Update image cache json file
-        with open("/mnt/P3/Development/Programmieren/Python/StreamController/src/windows/Store/cache/images.json", "w") as f:
+        with open("src/windows/Store/cache/images.json", "w") as f:
             json.dump(self.image_cache, f, indent=4)
 
         return img
@@ -180,7 +180,7 @@ class StoreBackend:
             self.api_cache[api_call_url] = {}
             self.api_cache[api_call_url]["answer"] = resp.json()
             self.api_cache[api_call_url]["time-code"] = datetime.now().strftime("%d-%m-%y-%H-%M")
-            with open("/mnt/P3/Development/Programmieren/Python/StreamController/src/windows/Store/cache/api.json", "w") as f:
+            with open("src/windows/Store/cache/api.json", "w") as f:
                 json.dump(self.api_cache, f, indent=4)
             return resp.json()
 

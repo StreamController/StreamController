80c8821fd0ee1f48f58feaa5a6ec3cc5f134c0db
Core447 <jmulmann@protonmail.com>
2023-11-26 11:44:00 +0100

    Add typing to globals

--- globals.py
+++ globals.py
@@ -1,10 +1,22 @@
-lm = None #LocaleManager
-media_manager = None #MediaManager
-asset_manager = None #AssetManager
-page_manager = None #PageManager
-settings_manager = None #SettingsManager
-app = None #App
-deck_manager = None #DeckManager
-plugin_manager = None #PluginManager
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.app import App
+    from locales.LocaleManager import LocaleManager
+    from src.backend.AssetManager import AssetManager
+    from src.backend.MediaManager import MediaManager
+    from src.backend.PageManagement.PageManager import PageManager
+    from src.backend.SettingsManager import SettingsManager
+    from src.backend.DeckManagement.DeckManager import DeckManager
+    from src.backend.PluginManager.PluginManager import PluginManager
+
+lm:"LocaleManager" = None
+media_manager:"MediaManager" = None #MediaManager
+asset_manager:"AssetManager" = None #AssetManager
+page_manager:"PageManager" = None #PageManager
+settings_manager:"SettingsManager" = None #SettingsManager
+app:"App" = None #App
+deck_manager:"DeckManager" = None #DeckManager
+plugin_manager:"PluginManager" = None #PluginManager
 video_extensions = ["mp4", "mov", "MP4", "MOV", "mkv", "MKV", "webm", "WEBM", "gif", "GIF"]
 image_extensions = ["png", "jpg", "jpeg"]

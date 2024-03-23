42b12a7097bd37ea5890def6eb47b1a4d6cef0c0
Core447 <jmulmann@protonmail.com>
2024-03-23 16:03:35 +0100

    Store data in .var/app

--- globals.py
+++ globals.py
@@ -3,8 +3,8 @@ import os
 from typing import TYPE_CHECKING
 import argparse
 
-DATA_PATH = os.path.join(os.getenv("XDG_CONFIG_HOME", os.path.expanduser("~/.config")), "StreamController")
-DATA_PATH = "data"
+DATA_PATH = os.path.join(os.path.expanduser("~"), ".var", "app", "com.core447.StreamController", "data")
+# DATA_PATH = "data"
 
 if TYPE_CHECKING:
     from src.app import App

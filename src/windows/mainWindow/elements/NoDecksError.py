104d689f7c8fc5879fc02ec7df16bd6974ee4ec9
Core447 <jmulmann@protonmail.com>
2024-03-26 17:45:05 +0100

    Fix some missong localizations

--- src/windows/mainWindow/elements/NoDecksError.py
+++ src/windows/mainWindow/elements/NoDecksError.py
@@ -45,10 +45,10 @@ class NoDecksError(Gtk.Box):
         self.build()
 
     def build(self):
-        self.no_pages_label = Gtk.Label(label="No Decks Available", css_classes=["error-label"])
+        self.no_pages_label = Gtk.Label(label=gl.lm.get("errors.no-deck.header"), css_classes=["error-label"])
         self.append(self.no_pages_label)
 
-        self.add_button = Gtk.Button(label="Add A Fake Deck", margin_top=60, css_classes=["error-resolve-button"],
+        self.add_button = Gtk.Button(label=gl.lm.get("errors.no-deck.add-fake"), margin_top=60, css_classes=["error-resolve-button"],
                                             hexpand=False, margin_start=60, margin_end=60, halign=Gtk.Align.CENTER)
         self.add_button.connect("clicked", self.on_add_click)
         self.append(self.add_button)

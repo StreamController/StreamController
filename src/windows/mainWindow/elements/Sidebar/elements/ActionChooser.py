d2bf59a3aac374642b3efa72643632e1122d9a18
Core447 <jmulmann@protonmail.com>
2024-03-28 13:07:56 +0100

    Localize ActionChooser Get More button

--- src/windows/mainWindow/elements/Sidebar/elements/ActionChooser.py
+++ src/windows/mainWindow/elements/Sidebar/elements/ActionChooser.py
@@ -109,7 +109,7 @@ class ActionChooser(Gtk.Box):
 
 class OpenStoreButton(Gtk.Button):
     def __init__(self, *args, **kwargs):
-        super().__init__(label="Get More", css_classes=["open-store-button"],
+        super().__init__(label=gl.lm.get("asset-chooser.add-more-button.label"), css_classes=["open-store-button"],
                          *args, **kwargs)
         self.connect("clicked", self.on_click)
 

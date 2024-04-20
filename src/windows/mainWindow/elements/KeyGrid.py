599a5cb2775848b9e933bde6f89ecc0e76895cbe
Core447 <jmulmann@protonmail.com>
2024-04-20 10:19:29 +0200

    Fix: KeyGrid pixbuf attr error

--- src/windows/mainWindow/elements/KeyGrid.py
+++ src/windows/mainWindow/elements/KeyGrid.py
@@ -271,8 +271,6 @@ class KeyButton(Gtk.Frame):
             if self.key_grid.deck_page.deck_stack_child.stack.get_visible_child() != self.key_grid.deck_page:
                 self.key_grid.deck_controller.ui_grid_buttons_changes_while_hidden[self.coords] = image
 
-        self.pixbuf = None
-        del self.pixbuf
         self.pixbuf = image2pixbuf(image.convert("RGBA"), force_transparency=True)
         self.show_pixbuf(self.pixbuf)
         # image.close()
@@ -282,6 +280,7 @@ class KeyButton(Gtk.Frame):
         # update righthand side key preview if possible
         if recursive_hasattr(gl, "app.main_win.sidebar"):
             self.set_icon_selector_previews(self.pixbuf)
+        self.pixbuf = None
 
     def set_icon_selector_previews(self, pixbuf):
         sidebar = gl.app.main_win.sidebar

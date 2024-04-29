d4cdc65c9fe36bee7b6f804a48fa1dd06ff40dea
Core447 <jmulmann@protonmail.com>
2024-04-29 19:12:53 +0200

    Add debug logs to MigrationManager

--- src/backend/Migration/MigrationManager.py
+++ src/backend/Migration/MigrationManager.py
@@ -1,5 +1,7 @@
 from src.backend.Migration.Migrator import Migrator
 
+from loguru import logger as log
+
 class MigrationManager:
     def __init__(self):
         self.migrators: list[Migrator] = []
@@ -11,7 +13,9 @@ class MigrationManager:
     def run_migrators(self):
         for migrator in self.get_ordered_migrators():
             if migrator.get_need_migration():
+                log.info(f"Running migrator to version app {migrator.app_version}")
                 migrator.migrate()
+                log.success(f"Successfully ran migrator to version app {migrator.app_version}")
 
     def get_ordered_migrators(self) -> list[Migrator]:
         return sorted(self.migrators, key=lambda migrator: migrator.parsed_app_version)

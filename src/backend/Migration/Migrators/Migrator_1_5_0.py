a447f0cab0e4ed4a508ab6a79bf85fdd365131ef
Core447 <jmulmann@protonmail.com>
2024-05-03 13:30:17 +0200

    Fix: Migrator crashing if no old pages present

--- src/backend/Migration/Migrators/Migrator_1_5_0.py
+++ src/backend/Migration/Migrators/Migrator_1_5_0.py
@@ -9,8 +9,53 @@ class Migrator_1_5_0(Migrator):
         super().__init__("1.5.0")
         
     def migrate(self):
-        # Change page properties
+        self.migrate_pages()
+        self.migrate_deck_settings()
+
+        # Update icons and wallapers to id system
+        if os.path.exists(os.path.join(gl.DATA_PATH, "icons", "Core447::Material Icons")):
+            os.rename(os.path.join(gl.DATA_PATH, "icons", "Core447::Material Icons"), os.path.join(gl.DATA_PATH, "icons", "com_core447_MaterialIcons"))
+
+        if os.path.exists(os.path.join(gl.DATA_PATH, "wallpapers", "Core447::Pixabay Favorites")):
+            os.rename(os.path.join(gl.DATA_PATH, "wallpapers", "Core447::Pixabay Favorites"), os.path.join(gl.DATA_PATH, "wallpapers", "com_core447_PixabayFavorites"))
+
+        self.set_migrated(True)
+
+    def migrate_deck_settings(self):
+        path = os.path.join(gl.DATA_PATH, "settings", "decks")
+        if not os.path.exists(path):
+            return
+        for deck_path in os.listdir(path):
+            if not deck_path.endswith(".json"):
+                continue
+            deck_path = os.path.join(gl.DATA_PATH, "settings", "decks", deck_path)
+            with open(deck_path, "r") as f:
+                deck = json.load(f)
+
+            background_path = deck.get("background", {}).get("path", "")
+            if background_path is None:
+                background_path = ""
+            if "Core447::Material Icons" in background_path:
+                deck["background"]["path"] = deck["background"]["path"].replace("Core447::Material Icons", "com_core447_MaterialIcons")
+            if "Core447::Pixabay Favorites" in background_path:
+                deck["background"]["path"] = deck["background"]["path"].replace("Core447::Pixabay Favorites", "com_core447_PixabayFavorites")
+
+            screensaver_path = deck.get("screensaver", {}).get("path", "")
+            if screensaver_path is None:
+                screensaver_path = ""
+            if "Core447::Material Icons" in screensaver_path:
+                deck["screensaver"]["path"] = deck["screensaver"]["path"].replace("Core447::Material Icons", "com_core447_MaterialIcons")
+            if "Core447::Pixabay Favorites" in screensaver_path:
+                deck["screensaver"]["path"] = deck["screensaver"]["path"].replace("Core447::Pixabay Favorites", "com_core447_PixabayFavorites")
+
+            with open(deck_path, "w") as f:
+                json.dump(deck, f, indent=4)
+
+    def migrate_pages(self):
         pages_dir = os.path.join(gl.DATA_PATH, "pages")
+        if not os.path.exists(pages_dir):
+            return
+        
         for page_path in os.listdir(pages_dir):
             if not page_path.endswith(".json"):
                 continue
@@ -59,38 +104,3 @@ class Migrator_1_5_0(Migrator):
 
             with open(page_path, "w") as f:
                 json.dump(page, f, indent=4)
-
-        for deck_path in os.listdir(os.path.join(gl.DATA_PATH, "settings", "decks")):
-            if not deck_path.endswith(".json"):
-                continue
-            deck_path = os.path.join(gl.DATA_PATH, "settings", "decks", deck_path)
-            with open(deck_path, "r") as f:
-                deck = json.load(f)
-
-            background_path = deck.get("background", {}).get("path", "")
-            if background_path is None:
-                background_path = ""
-            if "Core447::Material Icons" in background_path:
-                deck["background"]["path"] = deck["background"]["path"].replace("Core447::Material Icons", "com_core447_MaterialIcons")
-            if "Core447::Pixabay Favorites" in background_path:
-                deck["background"]["path"] = deck["background"]["path"].replace("Core447::Pixabay Favorites", "com_core447_PixabayFavorites")
-
-            screensaver_path = deck.get("screensaver", {}).get("path", "")
-            if screensaver_path is None:
-                screensaver_path = ""
-            if "Core447::Material Icons" in screensaver_path:
-                deck["screensaver"]["path"] = deck["screensaver"]["path"].replace("Core447::Material Icons", "com_core447_MaterialIcons")
-            if "Core447::Pixabay Favorites" in screensaver_path:
-                deck["screensaver"]["path"] = deck["screensaver"]["path"].replace("Core447::Pixabay Favorites", "com_core447_PixabayFavorites")
-
-            with open(deck_path, "w") as f:
-                json.dump(deck, f, indent=4)
-
-        # Update icons and wallapers to id system
-        if os.path.exists(os.path.join(gl.DATA_PATH, "icons", "Core447::Material Icons")):
-            os.rename(os.path.join(gl.DATA_PATH, "icons", "Core447::Material Icons"), os.path.join(gl.DATA_PATH, "icons", "com_core447_MaterialIcons"))
-
-        if os.path.exists(os.path.join(gl.DATA_PATH, "wallpapers", "Core447::Pixabay Favorites")):
-            os.rename(os.path.join(gl.DATA_PATH, "wallpapers", "Core447::Pixabay Favorites"), os.path.join(gl.DATA_PATH, "wallpapers", "com_core447_PixabayFavorites"))
-
-        self.set_migrated(True)

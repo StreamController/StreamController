863886ee8804c8ece4359ae5f4d23ac2404323ec
Core447 <jmulmann@protonmail.com>
2024-05-17 20:23:42 +0200

    Fix(ui): Adw version warning

--- src/windows/Store/ResponsibleNotesDialog.py
+++ src/windows/Store/ResponsibleNotesDialog.py
@@ -13,6 +13,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 import gi
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw
 
 import globals as gl

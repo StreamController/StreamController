a0c98c78bd983bfce3cc52fd4a4c17e623704034
Core447 <jmulmann@protonmail.com>
2024-05-17 16:08:10 +0200

    Feat(ui): Implement ui for custom stores

--- src/windows/Settings/Settings.py
+++ src/windows/Settings/Settings.py
@@ -15,6 +15,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 # Import gtk modules
 import gi
 
+from GtkHelper.GtkHelper import BetterPreferencesGroup
 from autostart import setup_autostart
 
 gi.require_version("Gtk", "4.0")
@@ -50,6 +51,8 @@ class Settings(Adw.PreferencesWindow):
         self.add(self.dev_page)
         self.add(self.performance_page)
         self.add(self.system_page)
+
+        self.set_visible_page(self.store_page)
 
     def load_json(self):
         # Load settings from file
@@ -194,33 +197,19 @@ class StorePageGroup(Adw.PreferencesGroup):
         self.auto_update = Adw.SwitchRow(title=gl.lm.get("settings-store-settings-auto-update"), active=True)
         self.add(self.auto_update)
 
-        self.use_custom_stores = Adw.SwitchRow(title=gl.lm.get("settings-store-settings-custom-stores"), active=False)
-        self.add(self.use_custom_stores)
-
-        self.use_custom_plugins = Adw.SwitchRow(title=gl.lm.get("settings-store-settings-custom-plugins"), active=False)
-        self.add(self.use_custom_plugins)
-
-        self.custom_stores = CustomContentGroup(title=gl.lm.get("settings-store-custom-stores-header"), margin_top=12)
+        self.custom_stores = CustomContentGroup(title=gl.lm.get("settings-store-custom-stores-header"), custom_type="stores", margin_top=12)
         self.add(self.custom_stores)
 
-        self.custom_plugins = CustomContentGroup(title=gl.lm.get("settings-store-custom-plugins-header"), margin_top=12)
+        self.custom_plugins = CustomContentGroup(title=gl.lm.get("settings-store-custom-plugins-header"), custom_type="plugins", margin_top=12)
         self.add(self.custom_plugins)
 
         self.load_defaults()
 
-        # Make Groups In-/Active
-        self.custom_stores.set_sensitive(self.use_custom_stores.get_active())
-        self.custom_plugins.set_sensitive(self.use_custom_plugins.get_active())
-
         # Connect signals
         self.auto_update.connect("notify::active", self.on_auto_update_toggled)
-        self.use_custom_stores.connect("notify::active", self.on_use_custom_stores_toggled)
-        self.use_custom_plugins.connect("notify::active", self.on_use_custom_plugins_toggled)
 
     def load_defaults(self):
         self.auto_update.set_active(self.settings.settings_json.get("store", {}).get("auto-update", True))
-        self.use_custom_stores.set_active(self.settings.settings_json.get("store", {}).get("use-custom-stores", False))
-        self.use_custom_plugins.set_active(self.settings.settings_json.get("store", {}).get("use-custom-plugins", False))
 
     def on_auto_update_toggled(self, *args):
         self.settings.settings_json.setdefault("store", {})
@@ -229,52 +218,68 @@ class StorePageGroup(Adw.PreferencesGroup):
         # Save
         self.settings.save_json()
 
-    def on_use_custom_stores_toggled(self, *args):
-        self.settings.settings_json.setdefault("store", {})
-        self.settings.settings_json["store"]["use-custom-stores"] = self.use_custom_stores.get_active()
-
-        self.settings.save_json()
-
-        self.custom_stores.set_sensitive(self.use_custom_stores.get_active())
-
-    def on_use_custom_plugins_toggled(self, *args):
-        self.settings.settings_json.setdefault("store", {})
-        self.settings.settings_json["store"]["use-custom-plugins"] = self.use_custom_plugins.get_active()
-
-        self.settings.save_json()
-
-        self.custom_plugins.set_sensitive(self.use_custom_plugins.get_active())
-
-class CustomContentGroup(Adw.PreferencesGroup):
-    def __init__(self, title: str, **kwargs):
+class CustomContentGroup(BetterPreferencesGroup):
+    def __init__(self, title: str, custom_type: str, **kwargs):
         super().__init__(title=title, **kwargs)
 
+        self.custom_type = custom_type
+        self.enable_key = f"enable-custom-{self.custom_type}"
+        self.store_key = f"custom-{self.custom_type}"
+
+        self.suffix_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        self.set_header_suffix(self.suffix_box)
+        
+        self.enable_switch = Gtk.Switch(valign=Gtk.Align.CENTER)
+        self.enable_switch.connect("state-set", self.on_toggle_enable)
+        self.suffix_box.append(self.enable_switch)
+
         self.add_button = Gtk.Button(icon_name="list-add-symbolic", css_classes=["flat"])
-        self.set_header_suffix(self.add_button)
+        self.add_button.connect("clicked", self.on_add_button_clicked)
+        self.suffix_box.append(self.add_button)
 
-        for i in range(100):
-            self.add(CustomContentEntry(url="https://PAIN", branch="main"))
+        self.load_config_values()
 
+    def on_toggle_enable(self, switch: Gtk.Switch, *args):
+        settings = gl.settings_manager.get_app_settings()
+        settings.setdefault("store", {})
+        settings["store"][self.enable_key] = switch.get_active()
 
-class CustomContentAdder(Adw.PreferencesRow):
-    def __init__(self):
-        super().__init__()
+        gl.settings_manager.save_app_settings(settings)
 
-        self.main_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, margin_start=10, valign=Gtk.Align.CENTER)
-        self.set_child(self.main_box)
+    def add_row(self, i: int, url: str, branch: str):
+        self.add(CustomContentEntry(content_group=self, i=i, url=url, branch=branch))
 
-        self.url = Adw.EntryRow(title="Repository URL", valign=Gtk.Align.CENTER)
-        self.main_box.append(self.url)
+    def load_config_values(self):
+        settings = gl.settings_manager.get_app_settings()
 
-        self.branch = Adw.EntryRow(title="Branch", text="main", valign=Gtk.Align.CENTER)
-        self.main_box.append(self.branch)
+        self.enable_switch.set_active(settings.get("store", {}).get(self.enable_key, False))
 
-        self.button_add = Gtk.Button(label="Add", valign=Gtk.Align.CENTER)
-        self.main_box.append(self.button_add)
+        for i, entry in enumerate(settings.get("store", {}).get(self.store_key, [])):
+            self.add_row(i, entry.get("url", ""), entry.get("branch", ""))
+
+    def on_add_button_clicked(self, *args):
+        settings = gl.settings_manager.get_app_settings()
+
+        settings.setdefault("store", {})
+        settings["store"].setdefault(self.store_key, [])
+        settings["store"][self.store_key].append({"url": None, "branch": None})
+
+        self.add_row(len(settings["store"][self.store_key]) - 1, None, None)
+
+        gl.settings_manager.save_app_settings(settings)
+
+    def update_indicies(self):
+        for i, row in enumerate(self.get_rows()):
+            row.i = i
 
 class CustomContentEntry(Adw.PreferencesRow):
-    def __init__(self, url: str, branch: str):
+    def __init__(self, content_group: CustomContentGroup, i: int, url: str, branch: str):
         super().__init__(activatable=False)
+
+        self.content_group = content_group
+        self.i = i
+        self.url = url
+        self.branch = branch
 
         self.main_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5, margin_start=5, margin_end=5)
         self.set_child(self.main_box)
@@ -283,9 +288,11 @@ class CustomContentEntry(Adw.PreferencesRow):
         self.main_box.append(self.entry_grid)
 
         self.url = Adw.EntryRow(title="Repository URL", valign=Gtk.Align.CENTER, text=url or "")
+        self.url.connect("changed", self.on_value_changed)
         self.entry_grid.append(self.url)
 
         self.branch = Adw.EntryRow(title="Branch", valign=Gtk.Align.CENTER, text=branch or "")
+        self.branch.connect("changed", self.on_value_changed)
         self.entry_grid.append(self.branch)
 
         self.button_remove = Gtk.Button(icon_name="user-trash-symbolic", valign=Gtk.Align.CENTER, css_classes=["destructive-action-on-hover", "flat"])
@@ -293,8 +300,30 @@ class CustomContentEntry(Adw.PreferencesRow):
 
         self.button_remove.connect("clicked", self.on_remove)
 
+    def on_value_changed(self, *args):
+        settings = gl.settings_manager.get_app_settings()
+
+        settings.setdefault("store", {})
+        settings["store"].setdefault(self.content_group.store_key, [])
+        settings["store"][self.content_group.store_key][self.i]["url"] = self.url.get_text()
+        settings["store"][self.content_group.store_key][self.i]["branch"] = self.branch.get_text()
+
+        gl.settings_manager.save_app_settings(settings)
+
     def on_remove(self, *args):
-        print("TODO: on_remove")
+        self.content_group.remove(self)
+
+        settings = gl.settings_manager.get_app_settings()
+        stores = settings.get("store", {}).get(self.content_group.store_key, [])
+        if self.i < len(stores):
+            stores.pop(self.i)
+
+        settings.setdefault("store", {})
+        settings["store"][self.content_group.store_key] = stores
+
+        gl.settings_manager.save_app_settings(settings)
+
+        self.content_group.update_indicies()
 
 
 class PerformancePage(Adw.PreferencesPage):

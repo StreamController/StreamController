11263d21c6ac34032f72c04269cc0babdf398cf7
Core447 <100139110+Core447@users.noreply.github.com>
2024-06-23 16:51:01 +0200

    Add GsakuL as a contributor

--- src/windows/mainWindow/elements/HeaderHamburgerMenuButton.py
+++ src/windows/mainWindow/elements/HeaderHamburgerMenuButton.py
@@ -105,6 +105,7 @@ class HeaderHamburgerMenuButton(Gtk.MenuButton):
         self.about.add_credit_section("Contributors", ["Core447 https://github.com/Core447"] + 
                                                        sorted(["coolapso https://github.com/coolapso",
                                                        "G4PLS https://github.com/G4PLS",
+                                                       "GsakuL https://github.com/GsakuL",
                                                        "mrintrepide https://github.com/mrintrepide"
                                                        "Sorunome https://github.com/Sorunome",
                                                        "ulrikstrid https://github.com/ulrikstrid",

160ab2c0646daaaa516d843eebb53a22fb8dd378
Core447 <100139110+Core447@users.noreply.github.com>
2024-06-30 17:47:07 +0200

    Chore: Add copyright header to ActionBase.py

--- src/backend/PluginManager/ActionBase.py
+++ src/backend/PluginManager/ActionBase.py
@@ -1,3 +1,17 @@
+"""
+Author: Core447
+Year: 2024
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This programm comes with ABSOLUTELY NO WARRANTY!
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 import threading
 import time
 from loguru import logger as log

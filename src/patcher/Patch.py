76ec759567fd7f45005f170249fc9ea6ed858c39
Core447 <100139110+Core447@users.noreply.github.com>
2024-09-21 09:23:24 +0200

    Fix: Not using new_enumerate with neo patch

---
+++ src/patcher/Patch.py
@@ -0,0 +1,3 @@
+class Patch:
+    def patch(self):
+        pass
--- src/patcher/patcher.py
+++ src/patcher/patcher.py
@@ -13,6 +13,10 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
+from src.patcher.patches.sd_neo.sd_neo import SDNeoPatch
+
+
 class Patcher:
     def patch(self):
-        from src.patcher.patches.sd_neo import sd_neo
+        neo = SDNeoPatch()
+        neo.patch()

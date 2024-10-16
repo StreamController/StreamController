8bad20f5e0c784ff93082ebe9c86bd9e8e409551
Core447 <100139110+Core447@users.noreply.github.com>
2024-10-16 17:51:57 +0200

    Update release notes

--- globals.py
+++ globals.py
@@ -117,7 +117,36 @@ logs: list[str] = []
 
 release_notes: str = """
 <ul>
-    <li>Fix: Ignoring choosen brightness for screensaver</li>
-    <li>Fix: Actions cannot change background color</li>
+    <li>Fix: Crash if label is not a string</li>
+    <li>Feat: Use git to download plugins in dev mode</li>
+    <li>Fix: Error launching action backend in terminal</li>
+    <li>Feat: Add Spanish translations</li>
+    <li>Fix: Swipes not working for Stream Deck Plus</li>
+    <li>Fix: Error when image size is 0</li>
+    <li>Fix: Error on X11 when decoding the active window</li>
+    <li>Fix: Not blocking action labels and images during screensaver</li>
+    <li>Fix: Not reloading page after plugin uninstall</li>
+    <li>Fix: Error when XDG_CURRENT_DESKTOP is not set</li>
+    <li>Fix: Font weights not stored</li>
+    <li>Fix: Removing action not updating input on active page</li>
+    <li>Fix: No always uninstalling plugins correctly</li>
+    <li>Fix: Crash when streamdeck-ui has no states key</li>
+    <li>Feat: Add option to change the outline color of labels</li>
+    <li>Feat: Add default font for labels</li>
+    <li>Fix: Registering dial and touch event when used to wake up</li>
+    <li>Feat: Add link to wiki when no decks are being detected</li>
+    <li>Fix: Not loading screen brightness from page</li>
+    <li>Fix: Ignoring font font styles and weights</li>
+    <li>Feat: New option to configure default font</li>
+    <li>Fix: Decks not always reconnecting</li>
+    <li>Fix: Error when renaming page to the same name</li>
+    <li>Fix: Keeping old page backups indefinitely</li>
+    <li>Feat: Add auto page change for swaywm</li>
+    <li>Feat: Add support for screensaver under Cinnamon</li>
+    <li>Fix: Crash when drag and dropping buttons with actions</li>
+    <li>Feat: Add ability to use line breaks in labels</li>
+    <li>Fix: Showing "No decks available" in header when no pages are available</li>
+    <li>Feat: Add basic support for the Stream Deck Neo (limited to the normal buttons)</li>
+    <li>Update dependencies</li>
 </ul>
 """

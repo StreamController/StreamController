9adbb299a8f91a283db041d5795a6651066b86a4
Core447 <100139110+Core447@users.noreply.github.com>
2024-10-16 16:56:46 +0200

    Add support for the sd-neo
    Squashed commit of the following:
    
    commit 6bd5ffda8b5e3a477eafcc6fb7fa6338f2a8bab7
    Author: Core447 <100139110+Core447@users.noreply.github.com>
    Date:   Sat Sep 21 09:29:07 2024 +0200
    
        Feat: Add new_enumerate info log
    
    commit 76ec759567fd7f45005f170249fc9ea6ed858c39
    Author: Core447 <100139110+Core447@users.noreply.github.com>
    Date:   Sat Sep 21 09:23:24 2024 +0200
    
        Fix: Not using new_enumerate with neo patch
    
    commit 1e717ed4619f5da37fbb13999ea836735be8afb0
    Author: Core447 <100139110+Core447@users.noreply.github.com>
    Date:   Tue Sep 10 07:56:04 2024 +0200
    
        Feat: Add basic sd-neo support via patcher WIP

---
+++ src/patcher/patcher.py
@@ -0,0 +1,22 @@
+"""
+Author: Core447
+Year: 2024
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This programm comes with ABSOLUTELY NO WARRANTY!
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from src.patcher.patches.sd_neo.sd_neo import SDNeoPatch
+
+
+class Patcher:
+    def patch(self):
+        neo = SDNeoPatch()
+        neo.patch()

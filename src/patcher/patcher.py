536a793d66a766550efae21d7ad306c81c7bf633
Core447 <100139110+Core447@users.noreply.github.com>
2024-11-13 19:47:50 +0100

    Fix: Old sd neo patch import

--- src/patcher/patcher.py
+++ src/patcher/patcher.py
@@ -13,9 +13,6 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
-from src.patcher.patches.sd_neo.sd_neo import SDNeoPatch
-
-
 class Patcher:
     def patch(self):
         return

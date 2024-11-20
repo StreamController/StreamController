cb00edf688077de30c3e30a1f4b3d44e4e394236
gapls <125091918+G4PLS@users.noreply.github.com>
2024-11-20 16:14:17 +0100

    Fix(Observer): Remove async loop errors that can occur by using try... catch... block

--- src/backend/PluginManager/AssetManagment/Observer.py
+++ src/backend/PluginManager/AssetManagment/Observer.py
@@ -4,6 +4,7 @@ Year: 2024
 """
 
 import asyncio
+from loguru import logger as log
 
 class Observer:
     def __init__(self):
@@ -18,9 +19,17 @@ class Observer:
             self.observers.remove(observer)
 
     def notify(self, *args, **kwargs):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(self._notify(*args, **kwargs))
+        try:
+            loop = asyncio.get_event_loop()
+            if loop.is_running():
+                asyncio.ensure_future(self._notify(*args, **kwargs))  # Schedule _notify as a coroutine
+            else:
+                loop.run_until_complete(self._notify(*args, **kwargs))
+            return
+        except:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            loop.run_until_complete(self._notify(*args, **kwargs))
 
     async def _notify(self, *args, **kwargs):
         coroutines = [self._ensure_coroutine(observer, *args, **kwargs) for observer in self.observers]
@@ -33,5 +42,5 @@ class Observer:
             try:
                 return await asyncio.to_thread(callback, *args, **kwargs)
             except Exception as e:
-                #log.error(f"Callback {callback.__name__} in {self.event_id} could not be called")
+                log.error(f"Callback {callback.__name__} in {self.event_id} could not be called")
                 return await asyncio.sleep(0)

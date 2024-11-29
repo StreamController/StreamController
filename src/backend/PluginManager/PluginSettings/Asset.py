cab7b1495d4c1a05af6fccd388cdce342db1d122
gapls <125091918+G4PLS@users.noreply.github.com>
2024-11-29 17:44:31 +0100

    Refactor(Asset): Removed some unneeded stuff and made it easier to declare assets

--- src/backend/PluginManager/PluginSettings/Asset.py
+++ src/backend/PluginManager/PluginSettings/Asset.py
@@ -5,9 +5,6 @@ from os.path import isfile
 
 class Asset:
     def __init__(self, *args, **kwargs):
-        self.change(*args, **kwargs)
-
-    def change(self, *args, **kwargs):
         pass
 
     def get_values(self):
@@ -17,17 +14,13 @@ class Asset:
         pass
 
     @classmethod
-    def from_json(cls, *args):
+    def from_json(cls, *args, **kwargs):
         return None
 
 class Color(Asset):
-    def __init__(self, *args, **kwargs):
-        self._color: tuple[int, int, int, int] = (0,0,0,0)
-
+    def __init__(self, color: tuple[int, int, int, int], *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def change(self, *args, **kwargs):
-        self._color = kwargs.get("color", (0,0,0,0))
+        self._color: tuple[int, int, int, int] = color
 
     def get_values(self):
         return self._color
@@ -36,24 +29,20 @@ class Color(Asset):
         return list(self._color)
 
     @classmethod
-    def from_json(cls, *args):
-        return cls(color=tuple(args[0]))
+    def from_json(cls, *args, **kwargs):
+        return cls(color=args[1])
 
 class Icon(Asset):
-    def __init__(self, *args, **kwargs):
-        self._icon: Media = None
-        self._rendered: Media = None
-        self._path: str = None
-
+    def __init__(self, path: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def change(self, *args, **kwargs):
-        path = kwargs.get("path", "")
-
         if isfile(path):
             self._path = path
             self._icon = Media.from_path(path)
             self._rendered = self._icon.get_final_media()
+        else:
+            self._path: str = None
+            self._icon: Media = None
+            self._rendered: Media = None
 
     def get_values(self):
         return self._icon, self._rendered
@@ -62,5 +51,5 @@ class Icon(Asset):
         return self._path
 
     @classmethod
-    def from_json(cls, *args):
+    def from_json(cls, *args, **kwargs):
         return cls(path=args[0])

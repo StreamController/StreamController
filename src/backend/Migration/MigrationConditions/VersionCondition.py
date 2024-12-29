f56c71352a6248a7d960bf92acfeca4165320305
gapls <125091918+G4PLS@users.noreply.github.com>
2024-12-29 22:41:52 +0100

    Feat(VersionCondition): Added logging to VersionCondition.py

--- src/backend/Migration/MigrationConditions/VersionCondition.py
+++ src/backend/Migration/MigrationConditions/VersionCondition.py
@@ -3,6 +3,7 @@ from packaging.version import Version
 from packaging import version
 
 from src.backend.Migration.MigrationCondition import MigrationCondition
+from loguru import logger as log
 
 class VersionCondition(MigrationCondition):
     def __init__(self, from_version: str, to_version: str, condition: Callable[[Version, Version], bool]):
@@ -11,4 +12,7 @@ class VersionCondition(MigrationCondition):
         self.condition: Callable[[Version, Version], bool] = condition
 
     def check(self) -> bool:
-        return self.condition(self.from_version, self.to_version)
+        log.info(f"COMPARING VERSIONS: {self.from_version} -> {self.to_version}")
+        success = self.condition(self.from_version, self.to_version)
+        log.info(f"VERSION CHECK SUCCESS: {success}")
+        return success

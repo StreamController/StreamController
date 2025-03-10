9290e91e50f00360b9e32acb15a1e3f7ddce470a
Core447 <100139110+Core447@users.noreply.github.com>
2025-03-10 17:51:58 +0100

    Update InputBases

--- src/backend/PluginManager/InputBases.py
+++ src/backend/PluginManager/InputBases.py
@@ -1,33 +1,39 @@
 from abc import ABC
 
-from backend.PluginManager.ActionCore import ActionCore
+from src.backend.DeckManagement.InputIdentifier import Input, InputIdentifier
+from src.backend.PluginManager.ActionCore import ActionCore
 
 class InputAction(ABC):
     pass
 
-class KeyAction(InputAction):
-    pass
+class KeyAction(InputAction, ActionCore):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+        self.add_event_assigner(
+            Input.Key.Events.DOWN,
+            self.on_trigger
+        )
 
 
 ###### Usage example
 
-class VolumeAction(ActionCore):
-    def increase():
-        print("Volume increased")
+# class VolumeAction(ActionCore):
+#     def increase():
+#         print("Volume increased")
 
-    def on_trigger(self, *args, **kwargs):
-        self.increase()
-
-
-class KeyVolumeAction(KeyAction, VolumeAction):
-    def __init__(self, action_id: str, action_name: str,
-                 deck_controller: "DeckController", page: "Page", plugin_base: "PluginBase", state: int,
-                 input_ident: "InputIdentifier"):
-        super().__init__(action_id, action_name, deck_controller, page, plugin_base, state, input_ident)
+#     def on_trigger(self, *args, **kwargs):
+#         self.increase()
 
 
-        self.add_event_assigner(
-            Input.Key.Events.DOWN,
-            self.on_trigger()
-        )
+# class KeyVolumeAction(KeyAction, VolumeAction):
+#     def __init__(self, action_id: str, action_name: str,
+#                  deck_controller: "DeckController", page: "Page", plugin_base: "PluginBase", state: int,
+#                  input_ident: "InputIdentifier"):
+#         super().__init__(action_id, action_name, deck_controller, page, plugin_base, state, input_ident)
+
+
+#         self.add_event_assigner(
+#             Input.Key.Events.DOWN,
+#             self.on_trigger()
+#         )

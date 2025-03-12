03b58911f00f1eaa738c717a3cd5064d3cc9ab8a
gapls <125091918+G4PLS@users.noreply.github.com>
2025-03-12 13:47:42 +0100

    Feat(SwitchRow): Made the SwitchRow implement auto translation for the title and subtitle

--- GtkHelper/GenerativeUI/SwitchRow.py
+++ GtkHelper/GenerativeUI/SwitchRow.py
@@ -7,22 +7,24 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from src.backend.PluginManager import ActionBase
 
-class ToggleRow(GenerativeUI[bool]):
-    def __init__(self, action_base: "ActionBase", var_name: str, default_value: bool, on_change: callable = None, title: str = None, subtitle: str = None):
+class SwitchRow(GenerativeUI[bool]):
+    def __init__(self, action_base: "ActionBase",
+                 var_name: str,
+                 default_value: bool,
+                 on_change: callable = None,
+                 title: str = None,
+                 subtitle: str = None):
         super().__init__(action_base, var_name, default_value, on_change)
 
-        self.widget = Adw.SwitchRow(
-            title=title,
-            subtitle=subtitle,
+        self.widget: Adw.SwitchRow = Adw.SwitchRow(
+            title=self.get_translation(title, title),
+            subtitle=self.get_translation(subtitle, subtitle),
             active=default_value
         )
         self.widget.connect("notify::active", self._value_changed)
 
     def _value_changed(self, switch, _):
         self._handle_value_changed(switch.get_active())
-
-    def get_ui(self) -> Gtk.Widget:
-        return self.widget
     
     def set_ui_value(self, value: bool):
         self.widget.set_active(value)

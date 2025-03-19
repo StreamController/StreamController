5bbc94033f4aaed57b394bf2bd7ce951837df4e8
gapls <125091918+G4PLS@users.noreply.github.com>
2025-03-19 14:11:01 +0100

    Feat(ActionConfigurator): Actually load the initial values when the UI gets built

--- src/windows/mainWindow/elements/Sidebar/elements/ActionConfigurator.py
+++ src/windows/mainWindow/elements/Sidebar/elements/ActionConfigurator.py
@@ -181,6 +181,8 @@ class ConfigGroup(Adw.PreferencesGroup):
             self.loaded_rows.append(row)
 
         for gen_ui in generative_ui_objects:
+            gen_ui.load_initial_ui_value()
+
             if not gen_ui.auto_add:
                 continue
 

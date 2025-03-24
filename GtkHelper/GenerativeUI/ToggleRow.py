c29d1428dddb18bcb0c52765f9d1af6e0eb2b043
GAPLS <125091918+G4PLS@users.noreply.github.com>
2025-03-24 21:18:59 +0000

    Feat: Added GenUI ToggleRow (#368)
    * Feat(GenUI): Added a ToggleRow
    
    * Feat(GenUI): Added a wrapper methods
    
    * Chore(ToggleRow): Rename get_toggle_amount to get_n_toggles
    
    ---------
    
    Co-authored-by: Core447 <100139110+Core447@users.noreply.github.com>

---
+++ GtkHelper/GenerativeUI/ToggleRow.py
@@ -0,0 +1,122 @@
+from GtkHelper.GenerativeUI.GenerativeUI import GenerativeUI
+
+import gi
+from gi.repository import Gtk, Adw
+
+from typing import TYPE_CHECKING
+
+from GtkHelper.GtkHelper import better_disconnect
+
+from GtkHelper.ToggleRow import ToggleRow as Toggle
+
+if TYPE_CHECKING:
+    from src.backend.PluginManager.ActionCore import ActionCore
+
+class ToggleRow(GenerativeUI[bool]):
+    def __init__(self, action_core: "ActionCore",
+                 var_name: str,
+                 default_value: int,
+                 toggles: list[Adw.Toggle] = None,
+                 title: str = None,
+                 subtitle: str = None,
+                 can_shrink: bool = True,
+                 homogeneous: bool = True,
+                 active: bool = True,
+                 on_change: callable = None,
+                 can_reset: bool = True,
+                 auto_add: bool = True,
+                 complex_var_name: bool = False
+                 ):
+        super().__init__(action_core, var_name, default_value, can_reset, auto_add, complex_var_name, on_change)
+
+        self._widget: Toggle = Toggle(
+            toggles = toggles,
+            active_toggle = self._default_value,
+            title=self.get_translation(title),
+            subtitle=self.get_translation(subtitle),
+            can_shrink=can_shrink,
+            homogeneous=homogeneous,
+            active=active
+        )
+
+        self._handle_reset_button_creation()
+        self.connect_signals()
+
+    def _handle_value_changed(self, new_value, update_settings: bool = True, trigger_callback: bool = True):
+        old_value = self.get_value()
+
+        if update_settings:
+            self.set_value(new_value)
+
+        if trigger_callback and self.on_change:
+            new_toggle = self._widget.get_toggle_at(new_value)
+            old_toggle = self._widget.get_toggle_at(old_value)
+
+            self.on_change(self.widget, new_toggle, old_toggle)
+
+    def connect_signals(self):
+        self.widget.toggle_group.connect("notify::active", self._value_changed)
+
+    def disconnect_signals(self):
+        better_disconnect(self.widget.toggle_group, self._value_changed)
+
+    def _value_changed(self, toggle_group, _):
+        index = self.widget.get_active_index()
+        self._handle_value_changed(index)
+
+    @GenerativeUI.signal_manager
+    def set_ui_value(self, value: int):
+        self.widget.set_active_toggle(value)
+
+    def reset_value(self):
+        self.widget.set_active_toggle(self._default_value)
+        self._handle_value_changed(self._default_value)
+
+    # Wrapper
+
+    def get_toggles(self):
+        return self.widget.get_toggles()
+
+    def get_n_toggles(self):
+        return self.widget.get_n_toggles()
+
+    def get_toggle_by_name(self, name: str):
+        return self.widget.get_toggle_by_name(name)
+
+    def get_toggle_at(self, index: int):
+        return self.widget.get_toggle(index)
+
+    def add_toggle(self, label = None, tooltip: str = None, icon_name: str = None, name: str = None, enabled: bool = True):
+        self.widget.add_toggle(label, tooltip, icon_name, name, enabled)
+
+    def add_toggles(self, toggles: list[Adw.Toggle]):
+        self.widget.add_toggles(toggles)
+
+    def add_custom_toggle(self, toggle: Adw.Toggle):
+        self.widget.add(toggle)
+
+    def set_active_toggle(self, index: int):
+        self.widget.set_active(index)
+
+    def set_active_by_name(self, name: str):
+        self.widget.set_active_name(name)
+
+    @GenerativeUI.signal_manager
+    def populate(self, toggles: list[Adw.Toggle], active_index: int):
+        self.widget.remove_all()
+        self.add_toggles(toggles)
+        self.widget.set_active(active_index)
+
+    def remove_toggle(self, toggle: Adw.Toggle):
+        self.widget.remove(toggle)
+
+    def remove_at(self, index: int):
+        toggle = self.widget.get_toggle_at(index)
+        self.widget.remove(toggle)
+
+    def remove_with_name(self, name: str):
+        toggle = self.widget.get_toggle_by_name(name)
+        self.widget.remove(toggle)
+
+    def remove_all(self):
+        self.widget.remove_all()
---
+++ GtkHelper/ToggleRow.py
@@ -0,0 +1,88 @@
+import gi
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Adw, Gio, GObject
+
+class ToggleRow(Adw.ActionRow):
+    def __init__(self,
+                 toggles: list[Adw.Toggle],
+                 active_toggle: int,
+                 title: str,
+                 subtitle: str,
+                 can_shrink: bool,
+                 homogeneous: bool,
+                 active: bool):
+        super().__init__(title=title, subtitle=subtitle)
+
+        self.toggle_group = Adw.ToggleGroup()
+        self.toggle_group.set_can_shrink(can_shrink)
+        self.toggle_group.set_homogeneous(homogeneous)
+        self.toggle_group.set_active(active)
+        self.toggle_group.set_valign(Gtk.Align.CENTER)
+
+        toggles = toggles or []
+        active_index = active_toggle or 0
+        self.populate(toggles, active_index)
+
+        self.add_suffix(self.toggle_group)
+
+    def get_toggles(self):
+        return self.toggle_group.get_toggles()
+
+    def get_n_toggles(self):
+        return self.toggle_group.get_n_toggles()
+
+    def get_toggle_by_name(self, name: str):
+        return self.toggle_group.get_toggle_by_name(name)
+
+    def get_toggle_at(self, index: int):
+        return self.toggle_group.get_toggle(index)
+
+    def get_active_toggle(self):
+        toggle_index = self.toggle_group.get_active()
+        toggle = self.toggle_group.get_toggle(toggle_index)
+        return toggle
+
+    def get_active_index(self):
+        return self.toggle_group.get_active()
+
+    def get_active_name(self):
+        return self.toggle_group.get_active_name()
+
+    def set_active_toggle(self, index: int):
+        self.toggle_group.set_active(index)
+
+    def set_active_by_name(self, name: str):
+        self.toggle_group.set_active_name(name)
+
+    def add_toggle(self, label = None, tooltip: str = None, icon_name: str = None, name: str = None, enabled: bool = True):
+        self.toggle_group.add(
+            Adw.Toggle(label=label, tooltip=tooltip, icon_name=icon_name, name=name, enabled=enabled)
+        )
+
+    def add_toggles(self, toggles: list[Adw.Toggle]):
+        for toggle in toggles:
+            self.toggle_group.add(toggle)
+
+    def populate(self, toggles: list[Adw.Toggle], active_index: int):
+        self.toggle_group.remove_all()
+        self.add_toggles(toggles)
+        self.toggle_group.set_active(active_index)
+
+    def add_custom_toggle(self, toggle: Adw.Toggle):
+        self.toggle_group.add(toggle)
+
+    def remove_toggle(self, toggle: Adw.Toggle):
+        self.toggle_group.remove(toggle)
+
+    def remove_at(self, index: int):
+        toggle = self.get_toggle_at(index)
+        self.toggle_group.remove(toggle)
+
+    def remove_with_name(self, name: str):
+        toggle = self.toggle_group.get_toggle_by_name(name)
+        self.toggle_group.remove(toggle)
+
+    def remove_all(self):
+        self.toggle_group.remove_all()

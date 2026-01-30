21be4b0453fd3fecd674979f4e0d8598980aad30
designgears <bobby@designgears.com>
2026-01-29 20:31:40 -0700

    Enhance TrayIcon functionality with GTK 4 support and icon handling improvements
    - Added GTK 4 and GDK imports to support modern UI features.
    - Updated icon setting logic to check for available assets and theme icons.
    - Refactored icon initialization to handle macOS compatibility.
    - Cleaned up unused code related to previous icon handling methods.
    - Improved image handling in DeckController to ensure UI updates are processed correctly.

--- src/tray.py
+++ src/tray.py
@@ -3,6 +3,11 @@ from loguru import logger as log
 import globals as gl
 from src.backend.trayicon import DBusTrayIcon, DBusMenu
 
+import gi
+gi.require_version("Gtk", "4.0")
+gi.require_version("Gdk", "4.0")
+from gi.repository import Gtk, Gdk
+
 class TrayIcon(DBusTrayIcon):
     MenuPath = "/com/core447/StreamController/Menu"
     IndicatorPath = "/org/ayatana/NotificationItem/com_core447_StreamController_TrayIcon"
@@ -20,7 +25,7 @@ class TrayIcon(DBusTrayIcon):
         self.menu.add_menu_item(6, menu_type="separator")
         self.menu.add_menu_item(7, "Quit", callback=self.on_quit)
         super().__init__(self.menu, self.MenuPath, self.IndicatorPath, self.AppId, "StreamController")
-        self.set_icon("com.core447.StreamController")
+        self.set_icon("com.core447.StreamController", "")
         self.set_tooltip("StreamController")
         self.set_label("StreamController")
 
@@ -33,11 +38,24 @@ class TrayIcon(DBusTrayIcon):
 
     @log.catch
     def initialize(self, main_win):
+        if gl.IS_MAC:
+            return
         self.main_win = main_win
         self.show_about_action = main_win.menu_button.open_about_action
         self.show_store_action = main_win.menu_button.open_store_action
         self.show_settings_action = main_win.menu_button.open_settings_action
         self.quit_app_action = main_win.menu_button.quit_action
+        assets = os.path.join(gl.top_level_dir, "Assets", "icons")
+        if os.path.isdir(os.path.join(assets, "hicolor")):
+            self.set_icon("com.core447.StreamController", os.path.abspath(assets))
+        else:
+            name = "com.core447.StreamController"
+            display = Gdk.Display.get_default()
+            if display:
+                theme = Gtk.IconTheme.get_for_display(display)
+                if theme.has_icon("streamcontroller") and not theme.has_icon(name):
+                    name = "streamcontroller"
+            self.set_icon(name, "")
         app_settings = gl.settings_manager.get_app_settings()
         show_now = app_settings.get("ui",{}).get("tray-icon", True)
         if show_now:
@@ -71,35 +89,3 @@ class TrayIcon(DBusTrayIcon):
     @log.catch
     def on_quit(self):
         self.quit_app_action.activate()
-
-
-
-# import os
-# os.environ["PYSTRAY_BACKEND"] = "appindicator"
-# # import pystray
-
-# from pystray import _appindicator
-# from PIL import Image
-# from pystray import Menu, MenuItem
-
-# class TrayIcon(_appindicator.Icon):
-#     def __init__(self):
-#         icon_image = Image.open("flatpak/icon_256.png")
-#         menu = Menu(
-#             MenuItem("StreamController", enabled=False, action=None),
-#             MenuItem("Open", self.on_open),
-#             MenuItem("Quit", self.on_quit)
-#         )
-#         super().__init__("StreamController", icon_image, menu=menu, title="StreamController")
-
-#     def on_open(self):
-#         print("Open")
-
-#     def on_quit(self):
-#         print("Quit")
-#         self.stop()  # Stop the icon running when "Quit" is clicked
-
-# if __name__ == "__main__":
-#     # Usage
-#     stream_controller_icon = TrayIcon()
-#     stream_controller_icon.run()

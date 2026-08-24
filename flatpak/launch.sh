0d6bb95bbb633bef7c127c5ff99d722f84252d18
Michael Hlas <3398654+mhlas7@users.noreply.github.com>
2026-08-24 09:50:12 -0700

    Fix: Add LD_LIBRARY_PATH to launch script for libportal runtime loading

--- flatpak/launch.sh
+++ flatpak/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export GI_TYPELIB_PATH=/app/lib64/girepository-1.0:/app/lib/girepository-1.0:$GI_TYPELIB_PATH
+export LD_LIBRARY_PATH=/app/lib64:/app/lib:$LD_LIBRARY_PATH
 cd /app/bin/StreamController
 python3 /app/bin/StreamController/main.py "$@"

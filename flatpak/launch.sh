c08ed52ad70d9c84d09650850d3e67e8fc0678c9
Michael Hlas <3398654+mhlas7@users.noreply.github.com>
2026-08-24 09:24:45 -0700

    Fix: Set GI_TYPELIB_PATH in flatpak launch script for Xdp namespace

--- flatpak/launch.sh
+++ flatpak/launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export GI_TYPELIB_PATH=/app/lib64/girepository-1.0:/app/lib/girepository-1.0:$GI_TYPELIB_PATH
 cd /app/bin/StreamController
 python3 /app/bin/StreamController/main.py "$@"
